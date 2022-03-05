ea0b91b6e6acfdba2332d1e6b0bc28df9d64c674
Ulises Santiago Caceres Licona <37051136+ulisesSan@users.noreply.github.com>
2022-03-05 21:20:56 +0100

    Comentarios

--- server/build/index.js
+++ server/build/index.js
@@ -5,16 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 class Server {
+    //el constructor ayuda a inicializar diferentes partes que se necesitan para el servidor 7u7
     constructor() {
         this.app = (0, express_1.default)();
         this.config();
         this.routes();
     }
+    //Se asigna un puerto para que funcione el servidor
     config() {
         this.app.set('port', process.env.PORT || 3000);
     }
     routes() {
     }
+    //Con el puerto asignado se le dice al modulo app que escuche un puerto por defecto o use el puerto tres mil
     start() {
         this.app.listen(this.app.get('port'), () => {
             console.log('Servidor en el pueto', this.app.get('port'));
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,13 +3,13 @@ import express, { Application } from 'express';
 class Server{
 
     public app: Application;
-
+    //el constructor ayuda a inicializar diferentes partes que se necesitan para el servidor 7u7
     constructor(){
         this.app = express();
         this.config();
         this.routes();
     }
-
+    //Se asigna un puerto para que funcione el servidor
     config(): void{
         this.app.set('port', process.env.PORT || 3000)
     }
@@ -17,7 +17,7 @@ class Server{
     routes(): void{
 
     }
-
+    //Con el puerto asignado se le dice al modulo app que escuche un puerto por defecto o use el puerto tres mil
     start(): void {
         this.app.listen(this.app.get('port'), () => {
             console.log('Servidor en el pueto', this.app.get('port'))
